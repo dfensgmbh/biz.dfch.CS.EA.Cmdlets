47ef57eb6ba9efceeda43c70cc0184be1f2b59ea
Ronald Rink <ronald.rink@d-fens.net>
2018-09-22 12:26:09 +0200

    GetObjectType
    * added cmdlet to resolve ObjectType values

---
+++ src/biz.dfch.CS.EA.Cmdlets/Constants/Help.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Copyright 2018 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace biz.dfch.CS.EA.Cmdlets.Constants
+{
+    public static class Help
+    {
+        public static class GetObjectType
+        {
+            public const string PARAMETER_VALUE = "Specifies the value of the ObjectType that is to be resolved.";
+            public const string PARAMETER_NAME = "Specifies the name or portion thereof of the ObjectType that is to be resolved.";
+            public const string PARAMETER_LIST_AVAILABLE = "Specifies that all defined ObjectTypes are to be returned.";
+        }
+    }
+}
---
+++ src/biz.dfch.CS.EA.Cmdlets/Convert/GetObjectType.cs
@@ -0,0 +1,105 @@
+﻿/**
+ * Copyright 2018 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Management.Automation;
+using biz.dfch.CS.EA.Cmdlets.Constants;
+using EA;
+
+namespace biz.dfch.CS.EA.Cmdlets.Convert
+{
+    [Cmdlet(
+         VerbsCommon.Get, "ObjectType"
+         ,
+         ConfirmImpact = ConfirmImpact.None
+         ,
+         DefaultParameterSetName = ParameterSets.DEFAULT
+         ,
+         SupportsShouldProcess = false
+         ,
+         HelpUri = "http://dfch.biz/biz/dfch/CS/EA/Cmdlets/Get-ObjectType/"
+    )]
+    [OutputType(typeof(ObjectType), ParameterSetName = new[] { ParameterSets.ID, ParameterSets.NAME })]
+    [OutputType(typeof(ObjectType[]), ParameterSetName = new[] { ParameterSets.DEFAULT, ParameterSets.NAME })]
+    public class GetObjectType : EnterpriseArchitectCmdletBase
+    {
+        public static class ParameterSets
+        {
+            public const string DEFAULT = nameof(DEFAULT);
+            public const string ID = nameof(ID);
+            public const string NAME = nameof(NAME);
+        }
+
+        [ValidateRange(0, int.MaxValue)]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = ParameterSets.ID, HelpMessage = Help.GetObjectType.PARAMETER_VALUE)]
+        [Alias("Id")]
+        public int Value { get; set; }
+
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = ParameterSets.NAME, HelpMessage = Help.GetObjectType.PARAMETER_NAME)]
+        public string Name { get; set; }
+
+        [PSDefaultValue(Value = true)]
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSets.DEFAULT, HelpMessage = Help.GetObjectType.PARAMETER_LIST_AVAILABLE)]
+        public SwitchParameter ListAvailable { get; set; } = true;
+
+        protected override void ProcessRecord()
+        {
+            if (ParameterSetName == ParameterSets.DEFAULT)
+            {
+                var list = Enum.GetNames(typeof(ObjectType))
+                    .Select(e => (ObjectType)Enum.Parse(typeof(ObjectType), e))
+                    .ToArray();
+                WriteObject(list);
+                return;
+            }
+
+            ObjectType result;
+            if (ParameterSetName == ParameterSets.ID)
+            {
+                var isValidValue = Enum.TryParse(Value.ToString(), true, out result);
+                Contract.Assert(isValidValue, Value.ToString());
+                Contract.Assert(Enum.GetValues(typeof(ObjectType)).Cast<int>().Contains(Value), Value.ToString());
+                WriteObject(result);
+                return;
+            }
+
+            Contract.Assert(ParameterSets.NAME == ParameterSetName);
+            if (Enum.TryParse(Name, true, out result))
+            {
+                WriteObject(result);
+                return;
+            }
+            
+            var names = Enum.GetNames(typeof(ObjectType))
+                .Where(e => -1 != e.IndexOf(Name, StringComparison.InvariantCultureIgnoreCase))
+                .Select(e => (ObjectType) Enum.Parse(typeof(ObjectType), e))
+                .ToArray();
+            if (1 == names.Length)
+            {
+                WriteObject(names[0]);
+            }
+            else
+            {
+                WriteObject(names);
+            }
+        }
+    }
+}
